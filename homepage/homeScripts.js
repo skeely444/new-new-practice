6f2808c2f7cf92e60e0835da029eae03ba515cc7
skeely444 <mauro.camerlinckx@telenet.be>
2025-04-15 19:14:56 +0200

    Darkmode script added

--- homepage/homeScripts.js
+++ homepage/homeScripts.js
@@ -4,3 +4,17 @@ const navMenu = document.getElementById("navMenu");
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+function darkMode() {
+  const clickedElement  = document.getElementById("toggler");
+  const styleSheet = document.getElementById("styler");
+  styleSheet.href = "homeDark.css";
+  clickedElement.innerHTML = ("Light mode?");
+  clickedElement.onclick = lightMode;
+};
+function lightMode () {
+  const clickedElement  = document.getElementById("toggler");
+  const styleSheet = document.getElementById("styler");
+  styleSheet.href = "homeLight.css";
+  clickedElement.innerHTML = ("Dark mode?");
+  clickedElement.onclick = darkMode;
+}
