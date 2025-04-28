f4542f67486a0280ce0d084a08629b0a0c71f384
skeely444 <mauro.camerlinckx@telenet.be>
2025-04-28 18:06:23 +0200

    I don't know what is going on whit my buttons...

--- login page/login.js
+++ login page/login.js
@@ -4,3 +4,14 @@ const navMenu = document.getElementById("navMenu");
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+function showMeYourPassword() {
+  const toggler = document.getElementById("theEye");
+  const inputField = document.getElementById("input");
+  toggler.innerHTML = '<i class="fa-solid fa-eye-slash"></i>'
+  inputField.type = "text";
+}
+function sendLogin() {
+  const inputtedPassword = document.getElementById("input").value
+  const inputtedUsername = document.getElementById("username").value
+  console.log(`The user his name is: ${inputtedUsername} and his password is: ${inputtedPassword}`)
+}
